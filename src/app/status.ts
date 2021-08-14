5015113a45d2c857ab4e3dc66d79ff763739d0da
Random Guy <leekhoi5319990@gmail.com>
2021-08-14 23:11:38 +0700

    enum

--- src/app/status.ts
+++ src/app/status.ts
@@ -4,3 +4,7 @@ export enum Status {
   SUCCEEDED,
   FAILED
 }
+
+/* Enums allow a developer to define a set of named constants.
+Using enums can make it easier to document intent, or create a set of distinct cases.
+TypeScript provides both numeric and string-based enums */
